561d63f93f2e6c7a2a822dab927a6017b1276a2d
taiseidev <taisei.developer@gmail.com>
2022-06-09 09:37:34 +0900

    依存性注入の学習とMVVM化（両方ともあまり理解できていない気がする）

--- lib/viewModel/chat_view_model.dart
+++ lib/viewModel/chat_view_model.dart
@@ -1,3 +1,4 @@
+import 'package:chat_functions_app/domain/send_message_for_someone.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -152,3 +153,7 @@ class ChatViewModel extends StateNotifier<List<types.Message>> {
 final isChattedProvider = StateProvider((_) => 0);
 // チャット送信済みかそうでないか
 final isSendedProvider = StateProvider((_) => 0);
+
+final SendMessageForSomeoneViewModelProvider = Provider<void>(
+  (ref) => ref.read(sendMessageForSomeoneUseCaseProvider).call(),
+);
