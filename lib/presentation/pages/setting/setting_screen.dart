90060e4ce3edc8b6c39551728e26e03063e033c8
taiseidev <taisei.developer@gmail.com>
2022-06-11 22:42:39 +0900

    設定画面を作成

--- lib/presentation/pages/setting/setting_screen.dart
+++ lib/presentation/pages/setting/setting_screen.dart
@@ -1,13 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('テスト'),
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: SettingsList(
+            physics: const NeverScrollableScrollPhysics(),
+            sections: [
+              SettingsSection(
+                title: Text('ユーザー情報'),
+                tiles: <SettingsTile>[
+                  SettingsTile.navigation(
+                    leading: const Icon(Icons.account_box_outlined),
+                    title: Text('プロフィール'),
+                    value: Text('編集'),
+                  ),
+                ],
+              ),
+              SettingsSection(
+                title: Text('設定'),
+                tiles: <SettingsTile>[
+                  SettingsTile.navigation(
+                    leading: Icon(Icons.info_outline),
+                    title: Text('お知らせ'),
+                    value: Text(''),
+                  ),
+                  SettingsTile.switchTile(
+                    onToggle: (value) {},
+                    initialValue: true,
+                    leading: Icon(Icons.notifications_outlined),
+                    title: Text('通知'),
+                  ),
+                ],
+              ),
+              SettingsSection(
+                title: Text('バージョン'),
+                tiles: <SettingsTile>[
+                  SettingsTile(
+                    leading: Icon(Icons.handyman),
+                    title: const Text('1.0.0'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
