4567be49b9fd594404a5e4da16d68b50c8ceb681
taiseidev <taisei.developer@gmail.com>
2022-06-11 23:16:30 +0900

    ThemeExtensionを使用できるように修正

--- lib/components/positive_or_negative_button.dart
+++ lib/components/positive_or_negative_button.dart
@@ -1,10 +1,26 @@
+import 'package:chat_functions_app/theme/positive_or_negative_button_style.dart';
 import 'package:flutter/material.dart';
 
 class PositiveOrNegativeButton extends StatelessWidget {
-  const PositiveOrNegativeButton({Key? key}) : super(key: key);
-
+  const PositiveOrNegativeButton({
+    Key? key,
+    this.style,
+    this.onPressed,
+  }) : super(key: key);
+  final PositiveOrNegativeButtonStyle? style;
+  final VoidCallback? onPressed;
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: () {}, child: Text(''));
+    final defaultStyle =
+        Theme.of(context).extension<PositiveOrNegativeButtonStyle>();
+    final title = style?.title ?? defaultStyle!.title;
+    final radius = style?.radius ?? defaultStyle!.radius;
+    final backgroundColor =
+        style?.backgroundColor ?? defaultStyle!.backgroundColor;
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(primary: backgroundColor),
+      onPressed: () {},
+      child: Text(title!),
+    );
   }
 }
