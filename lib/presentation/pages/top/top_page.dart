39e36a7dca806bf34ef03b273dcb4632bd0acb60
taiseidev <taisei.developer@gmail.com>
2022-06-11 22:09:17 +0900

    ログイン時にの書き込み処理をhome画面で行うように修正

--- lib/presentation/pages/top/top_page.dart
+++ lib/presentation/pages/top/top_page.dart
@@ -2,19 +2,13 @@ import 'package:chat_functions_app/main.dart';
 import 'package:chat_functions_app/presentation/pages/top/top_page_body.dart';
 import 'package:fcm_config/fcm_config.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class TopPage extends HookConsumerWidget {
+class TopPage extends StatelessWidget {
   const TopPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // 匿名認証のログインをtrueに
-    useEffect(() {
-      final loginState = ref.read(sharedPreferencesProvider);
-      loginState.setBool('loginState', true);
-    }, []);
+  Widget build(BuildContext context) {
     return FCMNotificationListener(
       onNotification: (RemoteMessage notification, void Function() setState) {},
       child: const Scaffold(
