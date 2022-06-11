9dad60dd554cf14c94f0334136ef9917b19e5c5c
taiseidev <taisei.developer@gmail.com>
2022-06-11 23:01:25 +0900

    「はい」か「いいえ」を表示するためのボタンを追加

---
+++ lib/components/positive_or_negative_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PositiveOrNegativeButton extends StatelessWidget {
+  const PositiveOrNegativeButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: () {}, child: Text(''));
+  }
+}
