4a6aff5d53d35e38f1e70d9bcc8bd15353eebf4e
onishi taisei <taisei.onishi@axia-works.com>
2022-06-14 22:34:11 +0900

    アナリティクス用のファイルを作成

---
+++ lib/data/service/firebase_analytics_service.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+
+// アナリティクスイベント
+enum AnalyticsEvent {
+  Button,
+}
+
+// 送りたいイベントがあったらこのクラスに列挙していく
+// アナリティクス
+class AnalyticsService {
+  static final analytics = FirebaseAnalytics.instance;
+  static final observer = FirebaseAnalyticsObserver(analytics: analytics);
+
+  // ボタンタップイベント送信
+  Future<void> sendButtonEvent({required String buttonName}) async {
+    sendEvent(
+      event: AnalyticsEvent.Button,
+      parameterMap: {'buttonName': buttonName},
+    );
+  }
+
+  // イベントを送信する
+  /// [event] AnalyticsEvent
+  /// [parameterMap] パラメータMap
+  Future<void> sendEvent({
+    required AnalyticsEvent event,
+    required Map<String, dynamic> parameterMap,
+  }) async {
+    final eventName = event.toString().split('.')[1];
+    analytics.logEvent(
+      name: eventName,
+      parameters: parameterMap,
+    );
+  }
+}
--- lib/presentation/pages/top/top_page.dart
+++ lib/presentation/pages/top/top_page.dart
@@ -1,11 +1,9 @@
 import 'package:chat_functions_app/presentation/pages/top/top_page_body.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:fcm_config/fcm_config.dart';
 import 'package:flutter/material.dart';
 
 class TopPage extends StatelessWidget {
-  TopPage({Key? key}) : super(key: key);
-  final analytics = FirebaseAnalytics.instance;
+  const TopPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
