3c94ba5fd5efa2cb47d629f0456ee6f9115ffa00
onishi taisei <taisei.developer@gmail.com>
2022-06-16 16:01:23 +0900

    トップページにlottieアニメーションを追加

--- lib/presentation/pages/top/top_page.dart
+++ lib/presentation/pages/top/top_page.dart
@@ -1,6 +1,7 @@
 import 'package:chat_functions_app/presentation/pages/top/top_page_body.dart';
 import 'package:fcm_config/fcm_config.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class TopPage extends StatelessWidget {
   const TopPage({Key? key}) : super(key: key);
@@ -11,7 +12,18 @@ class TopPage extends StatelessWidget {
       onNotification: (RemoteMessage notification, void Function() setState) {},
       child: Scaffold(
         resizeToAvoidBottomInset: false,
-        body: TopPageBody(),
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+            LottieBuilder.network(
+              'https://assets4.lottiefiles.com/packages/lf20_iyjr5y3a.json',
+              height: 1500,
+              // width: 1000,
+              fit: BoxFit.cover,
+            ),
+            TopPageBody(),
+          ],
+        ),
       ),
     );
   }
