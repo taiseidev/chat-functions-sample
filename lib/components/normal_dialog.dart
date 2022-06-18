17c1fbf7dda780a08b0d107e6d91e3f06cc591d7
onishi taisei <taisei.developer@gmail.com>
2022-06-18 19:30:36 +0900

    チャット送信時のダイアログを修正

--- lib/components/normal_dialog.dart
+++ lib/components/normal_dialog.dart
@@ -1,4 +1,3 @@
-import 'package:chat_functions_app/components/normal_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -17,11 +16,10 @@ class NormalDialog extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return AlertDialog(
-      title: const Text('誰かにチャット依頼を送信しますか？'),
-      content: const Text('相手が許可するとチャットルームが開かれます📩'),
+      title: Text(title ?? ''),
+      content: Text(content ?? ''),
       actions: <Widget>[
-        NormalButton(title: '送信 🚀', onTap: () async {}),
-        NormalButton(title: 'やめる 😢', onTap: () => Navigator.pop(context)),
+        actions ?? const SizedBox.shrink(),
       ],
     );
   }
