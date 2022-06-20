4ef63245630242ebb499875e17e230d81e452217
onishi taisei <taisei.developer@gmail.com>
2022-06-20 23:00:44 +0900

    normal_dialogを修正

--- lib/components/normal_dialog.dart
+++ lib/components/normal_dialog.dart
@@ -11,7 +11,7 @@ class NormalDialog extends ConsumerWidget {
 
   final String? title;
   final String? content;
-  final Column? actions;
+  final Widget? actions;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
